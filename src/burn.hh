758f67d4fe083487388899f60c06030151f5d0b5
Pavel Lepin <p.lepin@dyninno.com>
2017-11-07 12:18:52 +0200

    Wanted to reuse the service itself as a "static" resource, but this deadlocks for fairly obvious reasons.

--- src/burn.hh
+++ src/burn.hh
@@ -2,30 +2,36 @@
 
 async function send(string $to): Awaitable<string>
 {
+    print('Initiating request to [' . $to . ']...'."\n");
+
     $curl = curl_init();
     curl_setopt($curl, CURLOPT_URL, $to);
     $out = await \HH\Asio\curl_exec($curl);
     $result =  $out ? substr($out, 0, 100) : curl_error($curl);
     curl_close($curl);
 
-    print date('H-i-s').': '.$result.PHP_EOL;
+    print(date('H-i-s') . ': ' . $result . PHP_EOL);
 
     return $result;
 }
 
-if (isset($argv[1]) && isset($argv[2] && isset($argv[3]) && isset($argv[4]))) {
+// CLI remote service invoker
+if (isset($argv[1]) && isset($argv[2]) && isset($argv[3]) && isset($argv[4])) {
     $endpoint = $argv[1];
     $remoteService = $argv[2];
-    $attempts = $argv[3];
-    $threads = $argv[4];
+    $attempts = intval($argv[3]);
+    $threads = intval($argv[4]);
+
+    print('Working: ' . strval($attempts) . ' attempt(s) using ' . strval($threads) . ' threads to [' . $endpoint . ']...' . PHP_EOL);
 
     array_map(
         $_ ==>
             \HH\Asio\join(\HH\Asio\v(
                 array_map(
                     $_ ==> send($endpoint.'?'.urlencode($remoteService)),
-                    range(1, intval($threads))))),
-        range(1, intval($attempts)));
+                    range(1, $threads)))),
+        range(1, $attempts));
+// remote service that retrieves the remote resource specified by the URL in the QUERY_STRING
 } elseif (isset($_SERVER['QUERY_STRING'])) {
     \HH\Asio\join(send(urldecode($_SERVER['QUERY_STRING'])));
 }
