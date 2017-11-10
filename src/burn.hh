c0cee0edee5f8924a0782045edbef9dbadf6ee7b
Pavel Lepin <p.lepin@dyninno.com>
2017-11-10 12:51:54 +0200

    Trying to figure out whether using zero timeout for curl_multi_await() could prevent the crashes. Apparently not. Also, groovy effects on one of the runs, HHVM seems to hang, but spams the console with fatal errors on curl_multi_await() line -- but phpinfo.hh returns Service Unavailable as well. Groovy, as I said.

--- src/burn.hh
+++ src/burn.hh
@@ -5,10 +5,41 @@ async function send(string $to): Awaitable<string>
     print('Initiating request to [' . $to . ']...'."\n");
 
     $curl = curl_init();
+
     curl_setopt($curl, CURLOPT_URL, $to);
-    $out = await \HH\Asio\curl_exec($curl);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+    $mh = curl_multi_init();
+
+    curl_multi_add_handle($mh, $curl);
+
+    $active = -1;
+
+    do {
+        $ret = curl_multi_exec($mh, $active);
+    } while ($ret == CURLM_CALL_MULTI_PERFORM);
+
+    while ($active && $ret == CURLM_OK) {
+        $flag = await curl_multi_await($mh, 0.0);
+
+        if ($flag === -1) {
+            await \HH\Asio\usleep(100);
+        }
+
+        do {
+            $ret = curl_multi_exec($mh, $active);
+        } while ($ret == CURLM_CALL_MULTI_PERFORM);
+    }
+
+    $out = curl_multi_getcontent($curl);
+
     $result =  $out ? substr($out, 0, 100) : curl_error($curl);
+
+    curl_multi_remove_handle($mh, $curl);
+
     curl_close($curl);
+
+    curl_multi_close($mh);
 
     print(date('H-i-s') . ': ' . $result . PHP_EOL);
 
@@ -34,4 +65,7 @@ if (isset($argv[1]) && isset($argv[2]) && isset($argv[3]) && isset($argv[4])) {
 // remote service that retrieves the remote resource specified by the URL in the QUERY_STRING
 } elseif (isset($_SERVER['QUERY_STRING'])) {
     \HH\Asio\join(send(urldecode($_SERVER['QUERY_STRING'])));
+} else {
+    \HH\Asio\join(send('http://localhost'));
 }
+
